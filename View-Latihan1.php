c525d054cf98ea950aecedeb0c3818586bd38189
Rifki Muammar Adli <145312898+rifkimuammaradli@users.noreply.github.com>
2023-10-30 05:46:46 +0700

    Add files via upload

---
+++ View-Latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <title>Latihan 1</title>
+</head>
+<body>
+  Halo Teman-Teman.. Yuk kita belajar Web Programming..!!!<br>
+  Nilai Pertama = <?= $nilai1; ?><br>
+  Nilai Kedua = <?= $nilai2; ?><br>
+  Ini hasil dari pemodelan dengan metode penjumlahan yaitu <?=
+  $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
