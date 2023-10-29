2b21bca25c76c548d52dab8946e4947185ff4e60
Rifki Muammar Adli <145312898+rifkimuammaradli@users.noreply.github.com>
2023-10-30 00:14:59 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Rifki Muammar Adli
+ Saya tinggal di Ciputat
+ olahraga yang saya sukai adalah Bermain Bola.";
+ }
+}
